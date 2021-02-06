b7950e5741eefed509e2c3d0ed25c9012fcd06a3
shpilb <Boris.Shpilyuck@dell.com>
2021-02-07 01:16:14 +0200

    using Open API instead of SpringFox

--- mongodb-sleuth/src/main/java/org/users/controller/UserController.java
+++ mongodb-sleuth/src/main/java/org/users/controller/UserController.java
@@ -1,8 +1,5 @@
 package org.users.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.users.domain.User;
 import org.users.service.UsersService;
 
-import javax.validation.constraints.NotNull;
-import java.time.Instant;
 import java.util.List;
 
-@Api(value = "Users API", tags = "Users", description = "Users API")
 @RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
 @Controller
 @Slf4j
@@ -29,9 +22,9 @@ public class UserController {
 	@Autowired
 	private UsersService usersService;
 
-	@ApiOperation(value = "create user", response = User.class)
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<User> createUser(@ApiParam(name = "user", required = true) @RequestBody User user){
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<User> createUser(@RequestBody User user){
 		if (StringUtils.isEmpty(user.getId()) || usersService.exists(user.getId())) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
@@ -40,8 +33,7 @@ public class UserController {
 		return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);
 	}
 
-	@ApiOperation(value = "get users", response = User.class, responseContainer = "list")
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<List<User>> getUsers(){
 		List<User> users = usersService.findAll();
@@ -49,10 +41,9 @@ public class UserController {
 		return new ResponseEntity<>(users, httpHeaders, HttpStatus.OK);
 	}
 
-	@ApiOperation(value = "get user", response = User.class)
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public ResponseEntity<User> getUser(@NotNull @ApiParam(required = true) @PathVariable("id") String id){
+	public ResponseEntity<User> getUser(@PathVariable("id") String id){
 		if (!usersService.exists(id)) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
@@ -60,11 +51,10 @@ public class UserController {
 		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 
-	@ApiOperation(value = "update user", response = User.class)
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public ResponseEntity<User> updateUser(@NotNull @ApiParam(required = true) @PathVariable("id") String id,
-										   @ApiParam(name = "user", required = true) @RequestBody User user){
+	public ResponseEntity<User> updateUser(@PathVariable("id") String id,
+										   @RequestBody User user){
 		if (!id.equals(user.getId())) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
@@ -75,10 +65,9 @@ public class UserController {
 		return new ResponseEntity<>(updatedUser, HttpStatus.OK);
 	}
 
-	@ApiOperation(value = "delete user", response = ResponseEntity.class)
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping(value = "/{id}")
 	@ResponseBody
-	public ResponseEntity<HttpStatus> deleteUser(@NotNull @ApiParam(required = true) @PathVariable("id") String id){
+	public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") String id){
 		if (!usersService.exists(id)) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
